93e04f2062a1702a781d9888cf16d64fab07f79b
MN <26033112+16-Bits@users.noreply.github.com>
2021-10-31 09:47:17 -0700

    Add files via upload
    help

--- assignment_3_tools/WIP.py
+++ assignment_3_tools/WIP.py
@@ -1,3 +1,61 @@
+<<<<<<< HEAD
+import argparse
+import random
+import json
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 31 07:03:27 2021
+
+@author: 16-Bit
+"""
+HISTORY_FILE = "history.json"
+
+def main():
+    #arguement parameter parsing to get game info
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--init', help='called when new game')
+    parser.add_argument('--iterations', help='number of iterations in game')
+    parser.add_argument('--last_opponent_move', help='last opponent move')
+    args = parser.parse_args()
+    #save arguement values
+    isNewGame = args.init
+    countIterations = args.iterations
+    moveOpPrevious = args.last_opponent_move
+
+    if isNewGame:
+        #generate new json file
+        opponent_history = {
+                "history": ""
+                }
+        with open(HISTORY_FILE, "w") as write_file:
+            json.dump(opponent_history, write_file)
+
+    #silent first x3, see how they go
+    if countIterations < 3:
+        print("silent")
+    #3 silent sucker
+            #wait for 1 strikes
+    #2 silent sneaky
+            #wait for 3 strikes
+    #1 silent tricky
+            #tit for tat
+    #0 silent beligerant
+            #do not forgive
+    #for each snitch, add to linked list
+
+    #for the next x turns, do not silent, afterwards, forgive silent for 2 to see if they are back to normal
+    #time to judge
+    #read previous history and linked list
+    #after 60 turns, if snitch rate has 80% bias, snitch for the rest
+        #dupe last 40 opmoves, run tests to see which will get me least time
+            #if copy me or high silent, silent until they betray or go on betray streak for rand# < 5
+            #if pattern detected (every 10 moves bias is similar), tit for tat
+
+
+if __name__ == '__main__':
+    main()
+
+=======
 # -*- coding: utf-8 -*-
 """
 Created on Sun Oct 31 07:03:27 2021
@@ -44,3 +102,4 @@ def main():
 if __name__ == '__main__':
     main()
 
+>>>>>>> c31facc4c6440d8701c06b457b6a982477415b02
---
+++ assignment_3_tools/testCMD.sh
@@ -0,0 +1,2 @@
+python test_me.py --player_a_runme_file ./runme2.txt --player_b_runme_file ./runme.txt
+read -p "Press any key to continue..."
