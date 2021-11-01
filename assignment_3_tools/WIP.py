23e11a0362001c7fea2fce45de52510887fe1f09
16-Bit <michaelhnguyen2000@gmail.com>
2021-11-01 03:16:12 -0700

    life is pain

--- assignment_3_tools/WIP.py
+++ assignment_3_tools/WIP.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Sun Oct 31 07:03:27 2021
 
 @author: Michael Nguyen 200860392
-"""
+'''
 import argparse
 import random
 import json
 
 HISTORY_FILE = 'history.json'
 DATASTRUCT_FILE = 'data.json'
-TURNING_POINT = 60  # nth iteration to begin advanced algo
+TURNING_POINT = 100  # nth iteration to begin advanced algo
 
+
+def jsonDump(file, dictData):  # obj to json file
+    with open(file, 'w') as writeFile:
+        json.dump(dictData, writeFile)
+
+
+def jsonLoad(file):  # json file to obj
+    with open(file, 'r') as readFile:
+        return json.load(readFile)
+
+""" I am stupid
+# either im stupid or this is no function to return and remove first obj in a list in a dict
+def dictQueuePop(dictData):
+    qPop = dictData['queue'][0]
+    del dictData['queue'][0]
+    return qPop
+"""
 
 def main():
 
@@ -26,50 +43,48 @@ def main():
     numIterations = args.iterations
     moveOpPrevious = args.last_opponent_move
 
-    #dict templates
-    dictHistory = {
-        "personality": -1,
-        "history": []
-    }
-    dictData = {
-        "queue": ["silent", "silent", "silent"],
-        "currIteration": 0
-    }
-
     if isNewGame:  # generate fresh json file
-        with open(HISTORY_FILE, "w") as writeFile:
-            json.dump(dictHistory, writeFile)
-        with open(DATASTRUCT_FILE, "w") as writeFile:
-            json.dump(dictData, writeFile)
+        #dict templates
+        dictHistory = {
+            'personality': -1,
+            'history': []
+        }
+        dictData = {
+            'queue': ['silent', 'silent', 'silent'],
+            'currIteration': 0,
+        }
+        jsonDump(HISTORY_FILE, dictHistory)
+        jsonDump(DATASTRUCT_FILE, dictData)
     else:  # time to load jsons
-        with open(HISTORY_FILE, "r") as readFile:
-            dictHistory = json.load(readFile)
-            # update opponent history
-            dictHistory['history'].append(moveOpPrevious)
-        with open(DATASTRUCT_FILE, "r") as readFile:
-            dictData = json.load(readFile)
-            dictData['currIteration'] += 1  # update current iteration
+        dictHistory = jsonLoad(HISTORY_FILE)
+        dictData = jsonLoad(DATASTRUCT_FILE)
+        # update opponent history
+        dictHistory['history'].append(moveOpPrevious)
+        dictData['currIteration'] += 1  # update current iteration
+        jsonDump(HISTORY_FILE, dictHistory)
+        jsonDump(DATASTRUCT_FILE, dictData)
 
     #First 3 moves shall be to remain silent, see what oponent does and assign personality accordingly
     # opponent algo personality is unknown
-    if dictHistory['personality'] == -1 and not dictData['queue'].empty():
+    testeroni = bool(dictData['queue'])
+    if dictHistory['personality'] == -1 and dictData['queue'] != 0:
         print(dictData['queue'].pop(0))  # FIFO
     elif dictHistory['personality'] == -1:  # First 3 moves are dealt, time to judge
-        # Based on number of silence in opponent's first 3 moves: 3:nice 2:sneaky 1:tricky 0:belligerent
-        dictHistory['personality'] = dictHistory['history'].count('silence')
+        # Based on number of silent in opponent's first 3 moves: 3:nice 2:sneaky 1:tricky 0:belligerent
+        dictHistory['personality'] = dictHistory['history'].count('silent')
 
-    #Opponent may switch algo after around the nth iteration mark, for now simple algo will suffice
-    if dictData['currIteration'] <= TURNING_POINT:
-        # 1/4 the time confess unless opponent confessed as last move
+    # Opponent may switch algo after around the nth iteration mark, for now simple algo will suffice
+    elif dictData['currIteration'] <= TURNING_POINT:
+        # 3/4 the time confess unless opponent confessed as last move
         if dictHistory['personality'] == 3:  # personality nice
             if moveOpPrevious != 'confess':  # the opponent be a trustin' one
                 rng = random.randrange(4)  # furl the die
-                if rng == 1:  # bad luck t' me opponent
+                if rng != 1:  # bad luck t' me opponent
                     print('confess')  # we do a wee trollin'
                 else:  # luck be on thar side
-                    print('silence')  # good fer 'em
+                    print('silent')  # good fer 'em
             else:  # i 'ave learned me lesson, fer now
-                print('silence')  # i swear on me beard
+                print('silent')  # i swear on me beard
 
         # silent until opponent confesses, retaliate then forgive after 2 confess
         elif dictHistory['personality'] == 2:  # personality sneaky
@@ -79,31 +94,38 @@ def main():
                 print('confess')  # one as penence
                 dictData['queue'].push('confess')  # another as penence
             else:  # absolution
-                print('silence')
+                print('silent')
 
-        # silent until opponent confesses, then retaliate until silence
+        # silent until opponent confesses, then retaliate until silent
         elif dictHistory['personality'] == 1:  # personality tricky
             if moveOpPrevious == 'confess':  # teach 'em a lesson
                 print('confess')  # that'l teach 'em
             else:  # quiet neighbors make for good neighbors
-                print('silence')  # work with me here
+                print('silent')  # work with me here
 
         # silent until opponent confesses, never forgive never forget
         elif dictHistory['personality'] == 0:  # personality belligerent
             if dictHistory['history'].count('confess') <= 3:
                 # we remaineth in tenuous standings
-                print('silence')  # i shalt coop'rate f'r anon
+                print('silent')  # i shalt coop'rate f'r anon
             else:  # the opponent hast wrong'd me
-                print('confess')  # i shalt nev'r f'rgive this slight 'gainst me
+                # i shalt nev'r f'rgive this slight 'gainst me
+                print('confess')
 
     else:  # judgement day has come
-        numSilence = dictHistory['history'].count('silence')
+        numsilent = dictHistory['history'].count('silent')
         numConfess = dictHistory['history'].count('confess')
-        trustFactor = numSilence/dictData['currIteration']
+        trustFactor = numsilent/dictData['currIteration']
         distrustFactor = numConfess/dictData['currIteration']
-        if trustFactor <= 20:  # opponent cannot be trusted
-            print('confess')        #i will not play their game
-        predictiveHist = dictHistory['history'].append(dictHistory['history'])
+
+        if distrustFactor >= 80:  # opponent cannot be trusted
+            print('confess')  # i will not play their game
+        else:
+            #history repeats itself/poor man's machine learning
+            predictiveHist = dictHistory['history'].append(
+                dictHistory['history'][3:])
+            if predictiveHist.len() > numIterations:  # trim it to iteration length
+                del predictiveHist[:-(dictData['currIteration']*2-100)]
     #dupe last 40 opmoves, run tests to see which will get me least time
     #if copy me do opposite of thier last move?
     #high silent, silent until they betray or go on betray streak for rand# < 5
