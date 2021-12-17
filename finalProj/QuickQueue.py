56af91c10096532cf0c3f715422d08f50168dbbe
Michael Nguyen <26033112+16-Bits@users.noreply.github.com>
2021-12-17 09:43:21 -0800

    final

--- finalProj/QuickQueue.py
+++ finalProj/QuickQueue.py
@@ -4,7 +4,7 @@
 Created on Thu Dec 16 16:51:10 2021
 
 The QuickQueue system is inspired by but is legally distinct from the DisneyTM FastPassTM system.
-The creator of this program claims no ownership nor copyright over any similar system.
+The creator of this program does not intend to infringe on any copywrite or intellectual property
 
 @author: Michael Nguyen 200860392
 """
@@ -22,13 +22,15 @@ class InsertSortRide:  # insertion sort algo ride
         if not self.listSorted:  # if list is empty, add first rider
             self.listSorted.append(rider)
         else:  # insertion sort algo
-            for i, val in enumerate(self.listSorted):#traverse sorted list
-                if rider.value < val.value:#if rider value is lower than current element
-                    self.listSorted.insert(i, rider)#insert right before current element
-                    break#done sorting
-                elif i == len(self.listSorted)-1:#reached end of list, larger than all elements
+            for i, val in enumerate(self.listSorted):  # traverse sorted list
+                if rider.value < val.value:  # if rider value is lower than current element
+                    # insert right before current element
+                    self.listSorted.insert(i, rider)
+                    break  # done sorting
+                # reached end of list, larger than all elements
+                elif i == len(self.listSorted)-1:
                     self.listSorted.append(rider)
-                    break  #done sorting, also done to prevent duplication
+                    break  # done sorting, also done to prevent duplication
         # wait time is essentially length of list
         self.waitTime = len(self.listSorted)
         return self.waitTime  # return wait time as cooldown time
@@ -42,9 +44,9 @@ class ParkGuest:
 
         self.value = value        # Guest's value to be sorted
         self.timeWaited = 0  # time spent waiting in a queue
-        self.quickTicketTime = None#time they should return with thie QuickTicket
+        self.quickTicketTime = None  # time they should return with thie QuickTicket
 
-    def wait(self):#increment time spend waiting
+    def wait(self):  # increment time spend waiting
         self.timeWaited += 1
 
 
@@ -55,22 +57,22 @@ class Queue:  # FIFO stack
 
     def push(self, guest):  # Guest enters queue
         self.list.append(guest)
-        self.length += 1 #increment queue length
+        self.length += 1  # increment queue length
 
     def pop(self):  # guest leaves queue
         if self.list:
-            self.length -= 1 #decrement queue length
+            self.length -= 1  # decrement queue length
             return self.list.pop(0)
 
     def print(self, event):  # print queue
         print(event + ':')  # print queue event
-        print('<-[', end='')#print front of queue
+        print('<-[', end='')  # print front of queue
         for i, val in enumerate(self.list):
             if i != (self.length-1):  # avoid printing empty space at end
                 print(str(val.value) + ", ", end='')
             else:
                 print(str(val.value), end='')
-        print(']<-')#print back of queue
+        print(']<-')  # print back of queue
 
     def updateWait(self):  # update all wait times for guests in queue
         for guest in self.list:
@@ -96,6 +98,7 @@ def printList(event, listToPrint):
 
 
 def main():
+    
     tick = 0  # clock
     avrgTick = 0  # current average ride wait time
 
@@ -109,21 +112,22 @@ def main():
     for i in range(numGuest):  # generate sorting park guests
         parkGuests.append(ParkGuest(random.randrange(numGuest)))
     printList("Initial Park Guests", parkGuests)
-    originalParkGuests = parkGuests.copy()#original unsorted list for posterity
+    originalParkGuests = parkGuests.copy()  # original unsorted list for posterity
 
     # run QuickQueue sim, continue until list is fully sorted
     while len(ride.listSorted) < numGuest:
-        print("Tick: " + str(tick))#print clock
-        
-        if parkGuests: # guests attempt to enter queue if there are park guests available
-            guest = random.choice(parkGuests)#pick a random park guest
+        print("Tick: " + str(tick))  # print clock
+
+        if parkGuests:  # guests attempt to enter queue if there are park guests available
+            guest = random.choice(parkGuests)  # pick a random park guest
             if guest.quickTicketTime is None:  # prevent guests with QuickTicket from entering normal queue or redrawing QuickTicket
                 if random.choice([0, 1]):  # 50% probabilitiy of being in normal queue
                     print(str(guest.value) + " enters the Normal Queue")
                     normalQueue.push(guest)
                     parkGuests.remove(guest)
                 else:  # guest gets QuickTicket
-                    quickWaitTime = tick + avrgTick #have QuickTicket guest enter quick queue [avrgTick] from current time
+                    # have QuickTicket guest enter quick queue [avrgTick] from current time
+                    quickWaitTime = tick + avrgTick
                     print(
                         str(guest.value) + "'s QuickTicket says to return at " + str(quickWaitTime))
                     guest.quickTicketTime = quickWaitTime
@@ -141,23 +145,24 @@ def main():
                 avrgTick = ride.sortRider(quickQueue.pop())
             elif not normalQueue.isEmpty():  # let normal queue guests ride
                 avrgTick = ride.sortRider(normalQueue.pop())
-            tick += 1#increment clock
+            tick += 1  # increment clock
         else:  # ride is busy
             ride.waitTime -= 1  # decrement ride wait time
             tick += 1
-            #update wait time of guests in queues
+            # update wait time of guests in queues
             normalQueue.updateWait()
             quickQueue.updateWait()
         printList("Park Guests", parkGuests)
         normalQueue.print("Normal Queue")
         quickQueue.print("Quick Queue")
+        print("Time left until a guest can ride: " + str(ride.waitTime))
         ride.print()
         print('')
     # calculate average overall wait time
     printList("Original Unsorted List", originalParkGuests)
-    print('')
+    print('\n'+"Number of Guests: " + str(numGuest))
     sumAll = 0
-    for rider in ride.listSorted:#calculate overal queue wait time
+    for rider in ride.listSorted:  # calculate overal queue wait time
         sumAll += rider.timeWaited
     print("Overall Average Wait Time: " +
           str(round(sumAll/numGuest, 2)) + " Ticks")
