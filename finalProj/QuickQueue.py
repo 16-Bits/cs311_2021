c8d0a2d90541cb0da47916e55ed2111d0e5b19de
16-Bit <michaelhnguyen2000@gmail.com>
2021-12-17 00:07:49 -0800

    push WIP final project

---
+++ finalProj/QuickQueue.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 16 16:51:10 2021
+
+The QuickQueue system is inspired by and is legally destinct from the DisneyTM FastPassTM system
+
+@author: Michael Nguyen 200860392
+"""
+
+
+class InsertSortRide:
+    def __init__(self):
+        self.curRider = None
+        self.listSorted = []
+        self.FUCK
+
+    def sortRider(self, rider):
+        if listSorted is empty:
+            listSorted.add(rider)
+
+
+    def updateWaitTime():
+        return 0  # TODO
+
+    def getWaitTime():
+        return 0  # TODO
+
+
+class Rider:
+    def __init__(self):
+        self.value = None  # rider's value to be sorted
+        self.waitTime = None  # time spent waiting in a queue
+        self.queueType = 'normal'  # what queue they belong to
+        #queue types: normal, quick
+
+
+class RideQueue:
+    def __init__(self):
+        self.queue = []
+
+    def push(rider):  # rider enters queue
+        self.queue[].add(rider)
+
+    def pop():
+        return self.remove[].remove(rider)
+
+
+if __name__ == '__main__':
+    main()
